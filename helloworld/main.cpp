f2d4917834d3b54c916c59b2b9ceb044d8d7ec74
Yinsheng Li <YL@YZs-MacBook-Air.local>
2014-05-18 14:51:51 -0700

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Yinsheng Li on 5/18/14.
+//  Copyright (c) 2014 Yinsheng Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
